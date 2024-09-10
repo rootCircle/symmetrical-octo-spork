77b1e219240e2b59e0a6db478a85ca2581a9024b
Lab Rat <dev.frolics@gmail.com>
2024-09-10 13:35:46 +0530

    chore: fix issue template

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -3,5 +3,5 @@ import LLMEngineType from '@utils/llmEngineTypes';
 const SLEEP_DURATION: number = 2000;
 const EMPTY_STRING: string = '';
 const DEFAULT_LLM_MODEL = LLMEngineType.ChatGPT;
-const CURRENT_LLM_MODEL = LLMEngineType.Gemini;
+const CURRENT_LLM_MODEL = LLMEngineType.Ollama;
 export { EMPTY_STRING, SLEEP_DURATION, DEFAULT_LLM_MODEL, CURRENT_LLM_MODEL };
