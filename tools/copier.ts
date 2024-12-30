87b1b6644c960f30b6de9b0b6320fa83dd3b9fe7
Lab Rat <dev.frolics@gmail.com>
2024-12-30 19:29:19 +0530

    feat: enhance options page layout and implement efficient file copying

--- tools/copier.ts
+++ tools/copier.ts
@@ -25,3 +25,40 @@ export default async function copyContents(
     }
   }
 }
+
+/**
+ * Copies a file or creates a directory at the specified location.
+ * More efficient and cheap than copyContents() method.
+ *
+ * @param path - The source file or directory path.
+ * @param targetPath - The destination path for the copied file or directory.
+ *
+ * @remarks
+ * If the source path points to a directory, an empty directory is created at the target location.
+ * Otherwise, the file at the specified path is copied into the target location.
+ *
+ * @throws Will log an error if the source path cannot be accessed or if the file copy fails.
+ */
+export async function copyFileOrDirectory(path: string, targetPath: string) {
+  try {
+    const stats = await stat(path);
+    if (stats.isDirectory()) {
+      await mkdir(targetPath, { recursive: true });
+      return;
+    }
+  } catch (err) {
+    // biome-ignore lint/suspicious/noConsole: <explanation>
+    console.error(`Error statting path:${err}`);
+    return;
+  }
+
+  const targetDir = join(targetPath, '..');
+
+  try {
+    await mkdir(targetDir, { recursive: true });
+    await copyFile(path, targetPath);
+  } catch (error) {
+    // biome-ignore lint/suspicious/noConsole: <explanation>
+    console.error(`Error:${error}`);
+  }
+}
--- tools/watcher.ts
+++ tools/watcher.ts
@@ -1,10 +1,7 @@
 import chokidar from 'chokidar';
 import { runBuild } from './builder';
-import copyContents from './copier';
+import copyContents, { copyFileOrDirectory } from './copier';
 import { writeManifest } from './manifestWriter';
-import { copyFile, stat } from 'node:fs/promises';
-import { mkdir } from 'node:fs/promises';
-import { join } from 'node:path';
 
 const buildWatch = async () => {
   runBuild(true).catch(console.error);
@@ -15,32 +12,13 @@ const buildWatch = async () => {
   });
 
   const publicWatcher = chokidar.watch(['public/'], {
+    ignored: (path) => path === 'public/manifest.ts',
     persistent: true,
   });
 
   const handlePublicChange = async (path: string) => {
-    const targetFile = path.replace('public', 'build');
-    try {
-      const stats = await stat(path);
-      if (stats.isDirectory()) {
-        await mkdir(targetFile, { recursive: true });
-        return;
-      }
-    } catch (err) {
-      // biome-ignore lint/suspicious/noConsole: <explanation>
-      console.error(`Error statting path:${err}`);
-      return;
-    }
-    
-    const targetDir = join(targetFile, '..');
-    
-    try {
-      await mkdir(targetDir, { recursive: true });
-      await copyFile(path, targetFile);
-    } catch (error) {
-      // biome-ignore lint/suspicious/noConsole: <explanation>
-      console.error(`Error:${error}`);
-    }
+    await copyFileOrDirectory(path, path.replace('public', 'build'));
+    // await copyContents('./public', './build');
   };
 
   const handleManifestChange = async () => {
